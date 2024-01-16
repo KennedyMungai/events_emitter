eddf280b7b66ec43d37080bf857e7cb448da948a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 14:14:20 +0300

    Added some logic to the index js file

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const logEvents = require('./logEvents')
 
+
 const EventEmitter = require('events')
+
+
+class MyEmitter extends EventEmitter { }
+
+// Initialize the object
+const myEmitter = new MyEmitter()
+
+// Add a listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg))
+
+setTimeout(() => {
+    // Emit Event
+    myEmitter.emit('log', 'Log event emitted!')
+}, 2000)
