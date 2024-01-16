e08e6ee52612ff6cc093b3d9148ac5ab82d5109c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 14:08:57 +0300

    Made some updates to the js files on the project

--- logEvents.js
+++ logEvents.js
@@ -1,6 +1,21 @@
 const moment = require('moment')
-const {v4: uuid } = require('uuid')
+const { v4: uuid } = require('uuid')
 
 
-console.log(moment(new Date()).format('YYYY:MM:DD'))
-console.log(uuid())
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const logEvents = async (message) => {
+    const dateTime = `${moment(new Date()).format('LLLL')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+    console.log(logItem)
+
+    try {
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+module.exports = logEvents
