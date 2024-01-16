29acce2e5e16b3b38185428984f69ead83415e93
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 14:03:24 +0300

    Adde the path const

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {v4: uuid} = require('uuid')
 
 const fs = require('fs')
 const fsPromises = require('fs').promises
+const path = require('path')
 
 
 console.log(moment(new Date()).format('LLLL'))
